0288ed072448e87d4c65b6312cf45b22169a7d13
aquar <aquarene@nate.com>
2017-12-15 22:55:49 +0900

    1

--- Studys/src/NewsMachine.java
+++ Studys/src/NewsMachine.java
@@ -1,9 +1,82 @@
+import java.util.ArrayList;
 
-public class NewsMachine {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class NewsMachine implements Publisher{
+	private ArrayList<Observer> observers;
+	private String title;
+	private String news; 
+	
+	
+	
+	NewsMachine(){
+		observers = new ArrayList<>();
 	}
 
+	@Override
+	public void add(Observer observer) {
+		observers.add(observer);
+	}
+
+
+
+	@Override
+	public void delete(Observer observer) {
+		int index = observers.indexOf(observer);
+		observers.remove(index);
+		
+	}
+
+
+
+	@Override
+	public void notifyObserver() {
+		for(Observer observer : observers) {
+			observer.update(title, news);
+		}
+		
+	}
+	
+	
+	
+	public void setNewsInfo(String title, String news) {
+		this.title = title;
+		this.news = news;
+		notifyObserver();
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public String getNews(){
+		return news;
+	}
+	
+	public static void main(String[] args) {
+		String a ="aaa";
+		String b = "aaa";
+		
+		
+		System.out.println(a==b);
+		
+ 
+ 
+			
+ 
+	}
+	
+
+}
+
+
+class Outer {
+	Outer(){
+		
+	}
+	
+	class Inner{
+		int ivs = 100;
+		
+	}
+	public static class sInner{
+		int iv = 200;
+	}
 }
